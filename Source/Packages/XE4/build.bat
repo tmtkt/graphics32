c80ac7b262c5d46e7a9dbecde43ed4ef1e7d6013
JSchrtke <jschrtke@protonmail.com>
2021-06-25 15:34:29 +0200

    Add build script for XE4 package

---
+++ Source/Packages/XE4/build.bat
@@ -0,0 +1,8 @@
+dcc32.exe ^
+--no-config ^
+-B ^
+-NU..\..\ ^
+-LN..\..\ ^
+-NSSystem.Win;Vcl;Vcl.Samples;System;Soap;Winapi; ^
+-U"c:\program files (x86)\embarcadero\rad studio\11.0\lib\Win32\release"; ^
+GR32_R.dpk GR32_D.dpk
