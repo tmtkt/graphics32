55236d4ecd34e361183e985af8f6ebee9aba7e20
js <jschrtke@protonmail.com>
2021-09-21 13:40:50 +0200

    Update build script for usage with the package magician

--- Source/Packages/XE4/build.bat
+++ Source/Packages/XE4/build.bat
@@ -2,8 +2,9 @@ call rsvars.bat
 dcc32.exe ^
 --no-config ^
 -B ^
--NU..\..\ ^
--LN..\..\ ^
+-LE..\..\.. ^
+-LN..\..\.. ^
+-NU..\..\.. ^
 -NSSystem.Win;Vcl;Vcl.Samples;System;Soap;Winapi; ^
 -U"%BDS%\lib\Win32\release" ^
 GR32_R.dpk GR32_D.dpk
