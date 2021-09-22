ddb4e39b57c754cb6694c7f1452641099109c173
lars <lars.scheme@gmail.com>
2021-09-22 17:02:00 +0200

    Build XE4 bpl to root directory

--- Source/Packages/XE4/build.bat
+++ Source/Packages/XE4/build.bat
@@ -2,8 +2,8 @@ call rsvars.bat
 dcc32.exe ^
 --no-config ^
 -B ^
--NU..\..\ ^
--LN..\..\ ^
+-LE..\..\..\ ^
+-LN..\..\..\ ^
 -NSSystem.Win;Vcl;Vcl.Samples;System;Soap;Winapi; ^
 -U"%BDS%\lib\Win32\release" ^
 GR32_R.dpk GR32_D.dpk
